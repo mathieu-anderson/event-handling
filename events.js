7910fa205e69c17c9145c41e36007aad1c8a86d8
Mathieu <Mathieu.hj.anderson@gmail.com>
2016-10-25 09:40:01 +1300

    Add function Four

--- events.js
+++ events.js
@@ -9,6 +9,7 @@ function start () {
   one()
   two()
   three()
+  four()
   // Your turn! Create a new function called `two`, then call it from here.
 
 }
@@ -39,7 +40,10 @@ function three () {
 }
 
 // CREATE FUNCTION four HERE
-
+function four () {
+  var four = document.getElementById('four');
+  four.addEventListener('click', makeBlack);
+}
 // Changes the background color of event's target
 function makeBlue (evt) {
   evt.target.style.backgroundColor = 'blue'
@@ -55,4 +59,8 @@ function makeWhite (evt) {
 
 function makeOrange (evt) {
   evt.target.style.backgroundColor = 'orange'
+}
+
+function makeBlack(evt) {
+  evt.target.style.backgroundColor = 'black'
 }
